9167b094cccfc398c5e6de62f2c18d7e133e8b5f
Daniil <kashel369@bk.ru>
2022-05-06 07:29:11 +0300

    remove commented code

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,33 +1,3 @@
-// export default class UserInfo {
-//   constructor(nameSelector, subnameSelector, avatarSelector) {
-//     this._name = this._getElement(nameSelector)
-//     this._subname = this._getElement(subnameSelector)
-//     this._avatar = this._getElement(avatarSelector)
-//   }
-
-//   _getElement(selector, parentElem) {
-//     return parentElem ? parentElem.querySelector(selector) : document.querySelector(selector)
-//   }
-
-//   getUserInfo(api) {
-//     return api.getProfileFromServer()
-//   }
-//   setUserInfo({ name, about }, api) {
-//     this._name.textContent = name
-//     this._subname.textContent = about
-//     if (api) {
-//       return api.loadProfileOnServer({ name, about })
-//     }
-//   }
-
-//   setUserAvatar({ avatar }, api) {
-//     this._avatar.src = avatar
-//     if (api) {
-//       return api.loadAvatarOnServer({ avatar })
-//     }
-//   }
-// }
-
 export default class UserInfo {
   constructor(nameSelector, subnameSelector, avatarSelector) {
     this._name = document.querySelector(nameSelector)
@@ -41,18 +11,4 @@ export default class UserInfo {
     this._avatar.src = avatar
     this._id = _id
   }
-
-  // setUserInfo(name, about, avatar) {
-  //   const info = {}
-  //   if (name) {
-  //     info[name] = name
-  //   }
-  //   if (about) {
-  //     info[about] = about
-  //   }
-  //   if (avatar) {
-  //     info[avatar] = avatar
-  //   }
-  //   return info
-  // }
 }
